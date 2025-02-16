f9cf833c30e728b43da39a48436a53e29305db55
JoseRamonRenteroJimenez <jrentero@ucm.es>
2025-02-16 11:50:43 +0100

    Práctica 1 versión California - not working

--- PR1/Ejercicio1.py
+++ PR1/Ejercicio1.py
@@ -1,25 +1,29 @@
-import pandas as pd
 import numpy as np
-movies = pd.read_csv(r"C:\SI_24_25\PR1\movies_metadata.csv" ,dtype={ 10 : 'str'})
-credits = pd.read_csv(r"C:\SI_24_25\PR1\credits.csv")
-print(credits.keys())
-bruce = credits[credits["cast"] == ""]
-print(bruce)
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+#cargando el dataset de diabetes que incorpora sklearn.
+diabetes = datasets.load_diabetes()
+#usamos solo una caracteristica
+diabetes_x = diabetes.data[:,np.newaxis,2]
+#dividimos el dato ente entrenamiento y validación o test.
+#esto es algo recurrente cuando hacemos aprendizaje máquina.
 
-# Escribe aqui el ejercicio 1
-movies["id"] = movies["id"].astype(str)
-credits["id"] = credits["id"].astype(str)
-movie_credits = pd.merge(movies,credits,how='inner',on=('id'))
-actores = movie_credits[["title","cast"]].copy()
-#display(actores.head(2))
+diabetes_x_train = diabetes_x[:-20] # Todos menos los 20 ultimos.
 
-tested = 'bruce willis'
-#Hacemos una subselección de los dos campos que nos interesan y los copiamos a otro dataframe
+diabetes_x_test = diabetes_x[-20:] # desde el puesto 10 empezanod por le final hasta el final
 
-# Nos creamos una máscara con el método applymap que nos indica que campos contienen una condición que establecemos mediante una lambda
-mask = actores.applymap(lambda x:  tested.lower() in str(x).lower())
-# Aplicamos la mascara para encontrar que campos nos interesan usando la función any
-df1 = actores[mask.any(axis=1)]
-#Any nos devuelve la fila o columna (dependiendo del axis) que al menos tenga un campo a true de la máscara. Axis 1 indica
-# la columna en este caso. Si queremos devolver la fila, sería axis = 0
-display(df1)
+# luego cogemos las clases para obtener los valores esperados.
+diabetes_y_train = diabetes.target[:-20]
+diabetes_y_test = diabetes.target[-20:]
+
+#creamos la regresión lineal:
+linearreg = linear_model.LinearRegression()
+linearreg.fit(diabetes_x_train, diabetes_y_train)
+
+#Comprobamos la capacidad de predicción
+diabetes_y_pred = linearreg.predict(diabetes_x_test)
+print("Mostramos los valores obtenidos por la regresión lineal")
+print('Coeficientes: \n', linearreg.coef_)
+print("MSE: %.2f"
+      % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+print('R2: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred)) #coeficiente de regresión.
