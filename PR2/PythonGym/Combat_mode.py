fce7a0c1da35d9bf1a0904a58609c816cfd283a5
JoseRamonRenteroJimenez <jrentero@ucm.es>
2025-03-20 16:01:55 +0100

    Init PR3

--- PR2/PythonGym/Combat_mode.py
+++ PR2/PythonGym/Combat_mode.py
@@ -14,33 +14,24 @@ class Combat_mode(State):
         disparo_disponible = perception[config.P_CAN_FIRE]==1
         
         disparar=False
-        # accion = nada
         accion=-1
 
-        #si tenemos el disparo disponible ya no nos proporciona info el anterior 
         if(disparo_disponible):
             self.dir_shot = -1
 
         print("-------Inicio mirando a ",self.facing_dir," y mi último disparo fue hacia ",self.dir_shot)
 
-        #no sabemos donde esta el peligro
         direccion_peligro=-1
-        #ni su distancia
         dis_peligro=100
 
         for i in range (4):
-            #miramos que sea jugador o bala que no sea nuestra (por que tenemos la nuestra o por que viene de otra direccion a la que no hemos disparado)
             if(perception[i]==config.O_PLAYER or (perception[i]==config.O_SHELL and (disparo_disponible or (not disparo_disponible and i!=self.dir_shot)))):
                 if(perception[i+4]<dis_peligro):
-                    #encontramos el peligro más cercano y su direcciónd
                     dis_peligro=perception[i+4]
                     direccion_peligro=i
         
-        print("-------Peligro mas cercano en ",direccion_peligro+1, "a distancia ",dis_peligro)
-
         if(dis_peligro>-1 and direccion_peligro != self.facing_dir and disparo_disponible ):
             print("-------No posicionados")
-            #nos posicionamos y disparamos
             self.dir_shot=direccion_peligro
             self.facing_dir=direccion_peligro
             self.last_move=direccion_peligro+1
@@ -50,15 +41,11 @@ class Combat_mode(State):
 
         elif (dis_peligro>-1 and direccion_peligro == self.facing_dir and disparo_disponible):
             print("-------Bien posicionados")
-            #queremos no hacer nada
             accion=-1
             disparar = True
 
         else:
-            #hay sitio para quitarse de la trayectoria
             print("-------Modo huida")
-            #caso amenaza arriba o amenaza derecha
-            #por acciones de movimiento de tanque MOVE_UP = 1, MOVE_DOWN = 2, MOVE_RIGHT = 3,MOVE_LEFT = 4
             if(direccion_peligro%2 ==0):
                 if(perception[(direccion_peligro+2)%4+4] >=2):
                     accion=(direccion_peligro+2)%4+1
@@ -66,7 +53,6 @@ class Combat_mode(State):
                     accion=(direccion_peligro+3)%4+1
                 else:
                     accion=((direccion_peligro+1)%4)
-            #caso amenaza abajo o amenaza izquierda
             else:
                 if(perception[(direccion_peligro+1)%4+4] >=2):
                     accion=(direccion_peligro+1)%4+1
@@ -84,8 +70,6 @@ class Combat_mode(State):
     
     def Transit(self,perception):
         
-
-
         if(perception[config.P_NEIGHBORHOOD_UP]==config.O_PLAYER or perception[config.P_NEIGHBORHOOD_UP]==config.O_SHELL or
             perception[config.P_NEIGHBORHOOD_LEFT]==config.O_PLAYER or perception[config.P_NEIGHBORHOOD_LEFT]==config.O_SHELL or
             perception[config.P_NEIGHBORHOOD_DOWN]==config.O_PLAYER or perception[config.P_NEIGHBORHOOD_DOWN]==config.O_SHELL or
--- PR2/PythonGym/Explorer_mode.py
+++ PR2/PythonGym/Explorer_mode.py
@@ -9,53 +9,37 @@ class Explorer_mode(State):
         super().__init__(id)
 
     def Update(self, perception):
-        # Check if the agent can fire
         disparo_disponible = perception[config.P_CAN_FIRE] == 1
 
         if disparo_disponible:
             self.dir_shot = -1
 
         print("-------Modo explorer")
-        print("-------Inicio mirando a ", self.facing_dir, ", mi último disparo fue hacia ", self.dir_shot, "último movimiento a ", self.last_move)
         
         accion = 0
         disparo = False
         
-        # Get the distance and object in front of the agent
         distancia_delante = perception[self.facing_dir + 4]
         mirando_delante = perception[self.facing_dir]
 
-        # Calculate the new distance to the command center
         newCC_dist = sqrt((perception[config.P_AGENT_X] - perception[config.P_COMMAND_CENTER_X])**2 + (perception[config.P_AGENT_Y] - perception[config.P_COMMAND_CENTER_Y])**2)
-        
-        print("------- distancia a CC ", newCC_dist, "distancia anterior ", self.CC_dist, "diff =", self.CC_dist - newCC_dist)
-            
-        print("------- Mirando", mirando_delante, " a distancia ", distancia_delante)
-
-        # Check if the path ahead is clear and not blocked by unbreakable objects or other agents
+      
         if distancia_delante > config.C_EXPLO and mirando_delante != config.O_UNBREAKABLE:
             print("FM----- Free movement")
             accion = self.facing_dir + 1
             
-            # If the agent hasn't moved closer to the command center, change direction
             if self.CC_dist - newCC_dist < config.C_TOLERANCIA_MOVIMIENTO:
                 if self.facing_dir % 2 == 0:
                     accion = (self.facing_dir + 1) % 4 + 1
                 else:
                     accion = (self.facing_dir + 3) % 4 + 1
         else:
-            print("------- dist_bloqueo", abs(perception[config.P_AGENT_X] - perception[config.P_COMMAND_CENTER_X]), "bloqueo en", abs(config.C_EXPLO))
-            # If the last movement made the agent closer to the command center, keep moving in the same direction 2 of 3 times
-            # if self.CC_dist > newCC_dist and 3 > random.randint(1, 3):
             if self.CC_dist > newCC_dist:
                 print("KM----- Keep moving")
-                # Keep moving in the same direction
                 accion = self.facing_dir + 1
             else:
                 print("MA----- Movimiento aleatorio")
-                # If the agent hasn't moved closer to the command center, change direction
                 if self.CC_dist - newCC_dist < config.C_TOLERANCIA_MOVIMIENTO:
-                    # Check if the agent is in the same column as the command center
                     if abs(perception[config.P_AGENT_X] - perception[config.P_COMMAND_CENTER_X]) < abs(config.C_EXPLO):
                         print("MRV----- Restringido Vertical")
                         accion = random.randint(0, 1) + 1
@@ -65,19 +49,16 @@ class Explorer_mode(State):
                         else:
                             accion = (self.facing_dir + 3) % 4 + 1
                 else:
-                    # If the agent is in the same column as the command center
-                    if abs(perception[config.P_AGENT_X] - perception[config.P_COMMAND_CENTER_X]) < abs(config.C_EXPLO) and not(perception[self.facing_dir]==config.O_UNBREAKABLE and perception[self.facing_dir+4]<3):
+                    if abs(perception[config.P_AGENT_X] - perception[config.P_COMMAND_CENTER_X]) < abs(config.C_EXPLO) and not(perception[self.facing_dir] == config.O_UNBREAKABLE and perception[self.facing_dir + 4] < 3):
                         print("MRV----- Restringido Vertical")
                         accion = random.randint(0, 1) + 1
                     else:
                         accion = random.randint(0, 4) + 1
 
-        # Update the agent's facing direction and last move
         self.facing_dir = (accion + 3) % 4
         self.last_move = accion
         self.CC_dist = newCC_dist
         
-        # Check if the agent should fire
         if (mirando_delante == config.O_BRICK or mirando_delante == config.O_COMMAND_CENTER) and distancia_delante <= 2:
             disparo = True
             self.dir_shot = accion
@@ -85,7 +66,6 @@ class Explorer_mode(State):
         return accion, disparo
     
     def Transit(self, perception):
-        # Check if there are any players or shells in the neighborhood
         if (perception[config.P_NEIGHBORHOOD_UP] == config.O_PLAYER or perception[config.P_NEIGHBORHOOD_UP] == config.O_SHELL or
             perception[config.P_NEIGHBORHOOD_LEFT] == config.O_PLAYER or perception[config.P_NEIGHBORHOOD_LEFT] == config.O_SHELL or
             perception[config.P_NEIGHBORHOOD_DOWN] == config.O_PLAYER or perception[config.P_NEIGHBORHOOD_DOWN] == config.O_SHELL or
